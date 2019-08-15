614f936ce97efd6389d7cdb17776f77666d86f9a
Spencer Percy <spenperc@gmail.com>
2019-08-14 22:00:10 -0400

    -

--- Linalg/Linalg.cpp
+++ Linalg/Linalg.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <stdio.h>
 #include <string>
 using namespace std;
 
+#define N 4 
 
-
+void transpose(int A[][N], int B[][N]) 
+{ 
+    int i, j; 
+    for (i = 0; i < N; i++) 
+        for (j = 0; j < N; j++) 
+            B[i][j] = A[j][i]; 
+}  
 
 int main()
 {
